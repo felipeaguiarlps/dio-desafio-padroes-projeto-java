026455b4fa80bd34000748af2b5d5170ed61bbc4
Felipe Aguiar <96080744+felipeaguiarlps@users.noreply.github.com>
2022-05-31 13:55:11 -0300

    Add Strategy and Facade

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,9 +1,15 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
 public class Test {
     public static void main(String[] args) {
 
-        //Testes relacionados ao Design Pattern do Singleton
+        //Testes relacionados ao Design Pattern do Singleton:
 
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
@@ -23,6 +29,29 @@ public class Test {
         lazyHolder = SingletonLazyHolder.getInstance();
         System.out.println(lazyHolder);
 
+        System.out.println("---------------------");
+
+        // Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        System.out.println("-----------------");
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Felipe", "33211-23");
 
     }
 }
